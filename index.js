32e2fddb942d2597e09b09842a271dff8caac74b
Gurjit Singh <g.s@outlook.in>
2019-09-10 19:17:01 +0530

    fixes

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ module.exports = {
       impliedStrict: true,
       generators: true,
       objectLiteralDuplicateProperties: false
-    },
-    env: {
-      es6: true,
-      browser: true,
-      node: true
     }
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
   }
 }
